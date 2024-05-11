7c1ec2d77d564e4752f2e4af429f112cba7ce621
Alex Karanja <alexnjorge953@gmail.com>
2024-05-11 10:30:48 +0300

    nav changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import React from "react";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Home from "./components/Home";
+import Navbar from "./Navbar.jsx";
 
-function App() {
+const App = () => {
+  const current_theme = localStorage.getItem("current_theme");
+  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+
+  useEffect(() => {
+    localStorage.setItem("current_theme", theme);
+  }, [theme]);
+
   return (
-    <div>
+    <div className={'container ${theme}'}>
+      <Navbar theme={theme} setTheme={setTheme} />
       <Contact />
       <About />
       <Home />
     </div>
   );
-}
+};
 
 export default App;
